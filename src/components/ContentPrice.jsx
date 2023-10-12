d538a61c347e05b35cf106deeb763377e1059c67
Pabo Bernando <pabobernando69@gmail.com>
2023-10-12 17:45:13 +0700

    update partner

--- src/components/ContentPrice.jsx
+++ src/components/ContentPrice.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React from 'react'
 
 function ContentPrice() {
   return (
-    <div className="bg-white py-12 sm:py-16 md:py-20 lg:py-24 xl:py-32">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8 flex flex-col items-center justify-center">
-        <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-2xl font-semibold leading-8 text-gray-900 mb-8 sm:mb-10">
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <h2 className="text-center text-2xl font-semibold leading-8 text-gray-900">
           Partner
         </h2>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 text-center gap-4 sm:gap-6 md:gap-8 lg:gap-8 xl:gap-10">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-2xl font-bold">Ghany Wash</h1>
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-2xl font-bold">Ghany Kost</h1>
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-2xl font-bold">Ghany Homestay</h1>
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-2xl font-bold">GM GROUP</h1>
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://see.fontimg.com/api/renderfont4/BWK6B/eyJyIjoiZnMiLCJoIjo2NSwidyI6MTAwMCwiZnMiOjY1LCJmZ2MiOiIjMDAwMDAwIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/Z2hhbnkgd2FzaA/star7.png"
+            alt="Transistor"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://see.fontimg.com/api/renderfont4/3zdJL/eyJyIjoiZnMiLCJoIjo2NSwidyI6MTAwMCwiZnMiOjY1LCJmZ2MiOiIjMDAwMDAwIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/UFQgR2hhbnkgTXViYXJhcSBHcm91cA/galber-demo-regular.png"
+            alt="Reform"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://see.fontimg.com/api/renderfont4/z86B0/eyJyIjoiZnMiLCJoIjo2NSwidyI6MTAwMCwiZnMiOjY1LCJmZ2MiOiIjMDAwMDAwIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/Z2hhbnkgaG9tZXN0YXk/grinchmas.png"
+            alt="Tuple"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+            src="https://see.fontimg.com/api/renderfont4/qZDgr/eyJyIjoiZnMiLCJoIjo2NSwidyI6MTAwMCwiZnMiOjY1LCJmZ2MiOiIjMDAwMDAwIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/Z2hhbnkga29zdA/badrips.png"
+            alt="SavvyCal"
+            width={158}
+            height={48}
+          />
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default ContentPrice;
+export default ContentPrice
